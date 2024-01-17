dadfd05db0d10dd01344da9e116cd9832aff4b58
AlexRus245 <rastikhin2001@mail.ru>
2024-01-17 17:20:24 +0300

    Добавлены кнопки для самой игры, рейтинга игроков и кнопка выйти

--- Меню.py
+++ Меню.py
@@ -20,13 +20,34 @@ class MainMenu:
         self.click_sound = pygame.mixer.Sound("but_f.mp3")  # Музыка для клика на кнопки
         # Загружаем изображение для кнопки "Музыка"
         self.music_button_image_on = pygame.image.load(
-            "music_on.png").convert_alpha()  # изображение когда музыка играет
+            "music_on.png").convert_alpha()  # Изображение когда музыка играет
         self.music_button_image_on = pygame.transform.scale(self.music_button_image_on, (40, 40))
         self.music_button_image_off = pygame.image.load(
-            "music_off.png").convert_alpha()  # изображение когда музыка не играет
+            "music_off.png").convert_alpha()  # Изображение когда музыка не играет
         self.music_button_image_off = pygame.transform.scale(self.music_button_image_off, (40, 40))
         # Создаем кнопку "Музыка" с изображением
         self.music_button = Button(screen, None, 740, 550, 790, 600, self.music, image=self.music_button_image_on)
+        # Загружаем изображение для кнопки "Начать"
+        self.start_button_image = pygame.image.load("start.png").convert_alpha()
+        self.start_button_image = pygame.transform.scale(self.start_button_image, (150, 40))
+        self.start_button_image_active = pygame.image.load("start_active.png").convert_alpha()
+        self.start_button_image_active = pygame.transform.scale(self.start_button_image_active, (150, 40))
+        # Загружаем изображение для кнопки "Рейтинг"
+        self.rating_button_image = pygame.image.load("ratin.png").convert_alpha()
+        self.rating_button_image = pygame.transform.scale(self.rating_button_image, (150, 40))
+        self.rating_button_image_active = pygame.image.load("rating_active.png").convert_alpha()
+        self.rating_button_image_active = pygame.transform.scale(self.rating_button_image_active, (150, 40))
+        # Загружаем изображение для кнопки "Выйти"
+        self.exit_button_image = pygame.image.load("exitt.png").convert_alpha()
+        self.exit_button_image = pygame.transform.scale(self.exit_button_image, (150, 40))
+        self.exit_button_image_active = pygame.image.load("exit_active.png").convert_alpha()
+        self.exit_button_image_active = pygame.transform.scale(self.exit_button_image_active, (150, 40))
+        # Создаем кнопку "Начать"
+        self.start_button = Button(screen, None, 325, 250, 150, 35, self.start_game, image=self.start_button_image)
+        # Создаем кнопку "Рейтинг"
+        self.rating_button = Button(screen, None, 325, 300, 150, 35, self.show_rating, image=self.rating_button_image)
+        # Создаем кнопку "Выйти"
+        self.exit_button = Button(screen, None, 325, 350, 150, 35, self.exit_menu, image=self.exit_button_image)
 
     def update(self):
         self.handle_events()
@@ -34,6 +55,9 @@ class MainMenu:
     def draw(self):
         self.screen.blit(self.background_image, (0, 0))
         self.music_button.draw()
+        self.start_button.draw()
+        self.rating_button.draw()
+        self.exit_button.draw()
         pygame.display.flip()
 
     def handle_events(self):
@@ -44,8 +68,30 @@ class MainMenu:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Проверяем нажатие на кнопку
                 pos = pygame.mouse.get_pos()
-                if self.music_button.check_click(pos):
+                if self.music_button.check_click(pos):# Кнопка Музыка
                     self.music_button.command()
+                elif self.start_button.check_click(pos): # Кнопка Начать
+                    self.click_sound.play()
+                    self.start_button.command()
+                elif self.rating_button.check_click(pos):# Кнопка Рейтинг
+                    self.click_sound.play()
+                    self.rating_button.command()
+                elif self.exit_button.check_click(pos):# Кнопка Выйти
+                    self.exit_button.command()
+            if event.type == pygame.MOUSEMOTION:
+                pos = pygame.mouse.get_pos()
+                if self.start_button.check_click(pos):
+                    self.start_button_active(True)
+                else:
+                    self.start_button_active(False)
+                if self.rating_button.check_click(pos):
+                    self.rating_button_active(True)
+                else:
+                    self.rating_button_active(False)
+                if self.exit_button.check_click(pos):
+                    self.exit_button_active(True)
+                else:
+                    self.exit_button_active(False)
 
     def music(self):
         if self.music_on:
@@ -56,6 +102,40 @@ class MainMenu:
             pygame.mixer.music.unpause()
             self.music_button = Button(screen, None, 740, 550, 790, 600, self.music, image=self.music_button_image_on)
             self.music_on = True
+
+    def start_button_active(self, flag):
+        if flag:
+            self.start_button = Button(screen, None, 325, 250, 150, 35, self.start_game,
+                                       image=self.start_button_image_active)
+        else:
+            self.start_button = Button(screen, None, 325, 250, 150, 35, self.start_game,
+                                       image=self.start_button_image)
+
+    def rating_button_active(self, flag):
+        if flag:
+            self.rating_button = Button(screen, None, 325, 300, 150, 35, self.show_rating,
+                                        image=self.rating_button_image_active)
+        else:
+            self.rating_button = Button(screen, None, 325, 300, 150, 35, self.show_rating,
+                                        image=self.rating_button_image)
+
+    def exit_button_active(self, flag):
+        if flag:
+            self.exit_button = Button(screen, None, 325, 350, 150, 35, self.exit_menu,
+                                      image=self.exit_button_image_active)
+        else:
+            self.exit_button = Button(screen, None, 325, 350, 150, 35, self.exit_menu, image=self.exit_button_image)
+
+    def start_game(self):
+        pass
+
+    def show_rating(self):
+        pass
+
+    def exit_menu(self):
+        pygame.mixer.quit()
+        pygame.quit()
+        sys.exit()
 
 
 pygame.init()
